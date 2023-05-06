76c74f6dd08864935816df35c6786623a8128ff8
developertyrone <developertyrone@gmail.com>
2023-05-07 00:08:54 +0800

    update

--- leetcode/5_3sum[sort_twopointer].py
+++ leetcode/5_3sum[sort_twopointer].py
@@ -2,29 +2,88 @@ from typing import List
 
 
 class Solution:
+    # Solution 1
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
 
-        # for i,a in enumerate(nums):
-        #     if i > 0 and a == nums[i-1]:
-        #         continue
-        #     l, r = i+1, len(nums)-1
-        #     while l < r:
-        #         three_sum = a + nums[l] + nums[r]
-        #         if three_sum > 0:
-        #             r -= 1
-        #         elif three_sum < 0:
-        #             l += 1
-        #         else:
-        #             res.append([a, nums[l], nums[r]])
-        #             l += 1
-        #             while nums[l] == nums[l-1] and l < r:
-        #                 l += 1
+        for i,a in enumerate(nums):
+            print (i,a)
+            ## this handle for the
+            if i > 0 and a == nums[i-1]:
+                continue
 
-        for i,a in eumerates()
-
+            l,r = i+1, len(nums)-1
+            while l < r:
+                target = a + nums[l] + nums[r]
+                if target > 0:
+                    r -= 1
+                elif target < 0:
+                    l += 1
+                else:
+                    res.append([a,nums[l],nums[r]])
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
         return res
-s = Solution()
-result = s.threeSum([-1,0,1,2,-1,-4])
-print(result)
+
+    # Solution 2
+    def threeSum2(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        for i,a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
+                continue
+            self.twoSum(nums, i+1, len(nums)-1, -a, res)
+        return res
+
+    def twoSum(self, nums: List[int], l: int, r: int, target: int, res: List[List[int]]):
+        while l < r:
+            if nums[l] + nums[r] == target:
+                res.append([-target, nums[l], nums[r]])
+                l += 1
+                r -= 1
+                while l < r and nums[l] == nums[l-1]:
+                    l += 1
+            elif nums[l] + nums[r] < target:
+                l += 1
+            else:
+                r -= 1
+
+    # Solution 3
+    def threeSum3(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res, neg, pos, zero = [], [], [], []
+        for i in nums:
+            if (i < 0):
+                neg.append(i)
+            elif (i > 0):
+                pos.append(i)
+            else:
+                zero.append(i)
+
+        # for zero case
+        if (len(zero) >= 3):
+            res.append([0, 0, 0])
+
+        # for pure negative or positive list
+        if (len(neg) == 0 or len(pos) == 0):
+            return res
+
+        # for negative and positive list
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.threeSum([-1,0,1,2,-1,-4])
+    print(result)
+    result = s.threeSum([0,0,0,0])
+    print(result)
+    result = s.threeSum([0, 0, 0])
+    print(result)
+    result = s.threeSum([0, 0, 1])
+    print(result)
+    result = s.threeSum2([-1,0,1,2,-1,-4])
+    print(result)
